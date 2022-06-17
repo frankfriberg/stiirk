0ddfc2f0de6c314085238c7a35e314dd1fcb6147
frankfriberg <fra.friberg@gmail.com>
2022-06-17 21:08:43 +0200

    Added workot model and types

--- src/modules/workout/workout.model.ts
+++ src/modules/workout/workout.model.ts
@@ -1,1 +1,89 @@
-// TODO: #4 Add Workout model and API functionality
+import { Schema, model, models, Types, Model } from 'mongoose'
+import { IWorkout, RoundExerciseTypeEnum } from 'types/workout.types'
+
+const options = {
+  toJSON: { virtuals: true },
+}
+
+const WorkoutSchema = new Schema<IWorkout>(
+  {
+    title: {
+      type: String,
+      required: [true, 'Title cannot be empty'],
+    },
+    slug: {
+      type: String,
+      unique: true,
+      required: [true, 'Slug cannot be empty'],
+    },
+    timer: {
+      countdown: {
+        type: Boolean,
+        default: true,
+      },
+      countFrom: {
+        type: Number,
+        default: 3,
+      },
+      readExercise: {
+        type: Boolean,
+        default: true,
+      },
+    },
+    settings: {
+      setRest: {
+        type: Number,
+        default: 60,
+      },
+    },
+    sets: [
+      {
+        title: String,
+        exerciseRest: Number,
+        tabata: Boolean,
+        iterations: Number,
+        round: [
+          {
+            exercise: {
+              type: Types.ObjectId,
+              ref: 'Exercise',
+            },
+            custom: String,
+            type: {
+              type: String,
+              enum: [
+                RoundExerciseTypeEnum.Time,
+                RoundExerciseTypeEnum.Rep,
+                RoundExerciseTypeEnum.Length,
+              ],
+              default: RoundExerciseTypeEnum.Time,
+            },
+            amount: {
+              type: Number,
+              default: 30,
+            },
+            split: {
+              type: Boolean,
+              default: false,
+            },
+            splitBreak: {
+              type: Number,
+              default: 15,
+            },
+            splitTitle: {
+              type: [String],
+              default: ['Right', 'Left'],
+            },
+            excludeRead: Boolean,
+          },
+        ],
+      },
+    ],
+  },
+  options
+)
+
+const Workout: Model<IWorkout> =
+  models.Workout || model('Workout', WorkoutSchema)
+
+export default Workout
