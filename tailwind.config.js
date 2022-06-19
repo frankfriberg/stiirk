51b40cb8d444e0d9aaf76d4b7af2428abc58d0bc
frankfriberg <fra.friberg@gmail.com>
2022-06-19 11:37:06 +0200

    updated path for files to only include .tsx files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./src/pages/**/*.tsx', './src/components/**/*.tsx'],
   theme: {
     extend: {},
   },
