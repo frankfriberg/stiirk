a6dbeba5c265e56f92033b3dd46f1b5ae4f23f6f
frankfriberg <fra.friberg@gmail.com>
2022-09-03 23:02:10 +0200

    Added basic exercise routes with validation

--- src/server/router/exercise.ts
+++ src/server/router/exercise.ts
@@ -1,10 +1,119 @@
 import { z } from 'zod'
+import { createRouter } from './context'
+import { createProtectedRouter } from './protected-router'
 
 export const exerciseSchema = z.object({
   name: z.string(),
   slug: z.string(),
-  alias: z.array(z.string()).nullable(),
-  primary: z.array(z.string()).nullable(),
-  secondary: z.array(z.string()).nullable(),
-  force: z.enum(['push', 'pull']).nullable(),
+  alias: z.array(z.string()),
+  primary: z.array(z.string()),
+  secondary: z.array(z.string()),
 })
+
+export const exerciseRouter = createRouter()
+  .query('get-all', {
+    async resolve({ ctx }) {
+      return await ctx.prisma.exercise.findMany()
+    },
+  })
+  .query('get-by-id', {
+    input: z.string(),
+    async resolve({ input, ctx }) {
+      return await ctx.prisma.exercise.findUnique({
+        where: {
+          id: input,
+        },
+      })
+    },
+  })
+
+export const protectedExerciseRouter = createProtectedRouter()
+  .mutation('create', {
+    input: exerciseSchema,
+    async resolve({ input, ctx }) {
+      if (ctx.session.user.type !== 'ADMIN') return 403
+      return await ctx.prisma.exercise.create({
+        data: input,
+      })
+    },
+  })
+  .mutation('update-by-id', {
+    input: z.object({
+      id: z.string(),
+      data: exerciseSchema,
+    }),
+    async resolve({ input, ctx }) {
+      if (ctx.session.user.type !== 'ADMIN') return 403
+      return await ctx.prisma.exercise.update({
+        where: {
+          id: input.id,
+        },
+        data: input.data,
+      })
+    },
+  })
+
+export const requestRouter = createProtectedRouter()
+  // Creates new request for a new exercise to be added
+  .mutation('create', {
+    input: exerciseSchema,
+    async resolve({ input, ctx }) {
+      return await ctx.prisma.exerciseRequest.create({
+        data: {
+          ...input,
+          type: 'CREATE',
+          user: {
+            connect: {
+              id: ctx.session.user.id,
+            },
+          },
+        },
+      })
+    },
+  })
+  // Creates new request to update an existing exercise
+  .mutation('update', {
+    input: exerciseSchema,
+    async resolve({ input, ctx }) {
+      return await ctx.prisma.exerciseRequest.create({
+        data: {
+          ...input,
+          type: 'UPDATE',
+          user: {
+            connect: {
+              id: ctx.session.user.id,
+            },
+          },
+        },
+      })
+    },
+  })
+  // Returns all requests
+  .query('getAll', {
+    async resolve({ ctx }) {
+      if (ctx.session.user.type !== 'ADMIN') return 403
+      return await ctx.prisma.exerciseRequest.findMany()
+    },
+  })
+  // Returns a single request by ID
+  .query('getById', {
+    input: z.string(),
+    async resolve({ input, ctx }) {
+      return await ctx.prisma.exerciseRequest.findUnique({
+        where: {
+          id: input,
+        },
+      })
+    },
+  })
+  // Archives a single request by ID
+  .mutation('archiveById', {
+    input: z.string(),
+    async resolve({ input, ctx }) {
+      if (ctx.session.user.type !== 'ADMIN') return 403
+      return await ctx.prisma.exerciseRequest.update({
+        where: { id: input },
+        data: { archived: true },
+      })
+    },
+  })
