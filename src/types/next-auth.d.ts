4a4cbcea32fe834d744e54aed981e65d4415e8a1
frankfriberg <fra.friberg@gmail.com>
2022-09-04 22:25:26 +0200

    Added isAdmin and isEditor to session context

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -7,7 +7,8 @@ declare module 'next-auth' {
   interface Session {
     user?: {
       id: string
-      type: string
+      isAdmin: boolean
+      isEditor: boolean
     } & DefaultSession['user']
   }
 }
